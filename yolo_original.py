0c599901874ad0f71d50dad3718ffd7477ab75d0
levywangx <147551403+levywangx@users.noreply.github.com>
2025-07-20 22:07:30 +0700

    Update yolo_original.py

--- yolo_original.py
+++ yolo_original.py
@@ -9,11 +9,20 @@ results = model("images/dog_test.jpg")
 
 # ---- RESULTS ---- #
 results[0].show()
-results[0].save(filename="results/dog_detected.jpg")
+# results[0].save(filename="results/dog_detected.jpg")
+
+for result in results:
+    boxes = result.boxes
+    for box in boxes:
+        cls_id = int(box.cls[0])
+        class_name = model.names[cls_id]
+        confidence = float(box.conf[0])
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        print(f"Detected : {class_name} ({confidence:.2f}) at [{x1}, {y1}, {x2}, {y2}]")
 
 # ---- DETECT FROM CAMERA OR VIDEO ---- #
 # cap = cv2.VideoCapture(0)
-# cap = cv2.VideoCapture("video.mp4")
+# cap = cv2.VideoCapture("")
 
 # while True:
 #     ret, frame = cap.read()
