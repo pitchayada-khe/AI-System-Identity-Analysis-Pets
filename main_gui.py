ec863fc0648c9547b00bc073078cb2e8b5035b5c
levywangx <147551403+levywangx@users.noreply.github.com>
2026-03-21 23:35:23 +0700

    update GUI version 2

--- main_gui.py
+++ main_gui.py
@@ -7,6 +7,8 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
+from PyQt5.QtWidgets import QScrollArea
+from datetime import datetime
 
 from utils.detection_model import detection
 from utils.identification_model import identification
@@ -49,6 +51,61 @@ class CameraWorker(QThread):
         self.quit()
         self.wait()
 
+# ---- CARD WIDGET LOGS ---- #
+class DetectionCard(QWidget):
+    def __init__(self, image, label, timestamp):
+        super().__init__()
+
+        self.setFixedSize(140, 150)
+        self.setStyleSheet("""
+            background-color: white;
+            border-radius: 10px;
+            border: 1px solid #ddd;
+        """)
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(8, 8, 8, 8)
+        layout.setSpacing(4)
+
+        # ---- Image ---- #
+        img_label = QLabel()
+        img_label.setFixedSize(124, 90)
+        img_label.setAlignment(Qt.AlignCenter)
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        h, w, ch = image.shape
+        qt_img = QImage(image.data, w, h, ch * w, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(qt_img).scaled(
+            124, 90, Qt.KeepAspectRatio, Qt.SmoothTransformation
+        )
+        img_label.setPixmap(pixmap)
+
+        # ---- Class Label ---- #
+        class_label = QLabel(f"Class : {label.upper()}")
+        class_label.setAlignment(Qt.AlignCenter)
+        class_label.setStyleSheet("""
+            font-size: 12px;
+            font-weight: bold;
+        """)
+
+        # ---- Timestamp ---- #
+        time_label = QLabel(f"Time : {timestamp}")
+        time_label.setAlignment(Qt.AlignCenter)
+        time_label.setStyleSheet("""
+            font-size: 10px;
+            font-weight: bold;
+            background-color: #4CAF50;
+            color: white;
+            padding: 2px 6px;
+            border-radius: 6px;
+        """)
+
+        layout.addWidget(img_label)
+        layout.addWidget(class_label)
+        layout.addWidget(time_label)
+
+        self.setLayout(layout)
+
 
 # ---- MAIN GUI ---- #
 class MainWindow(QWidget):
@@ -56,6 +113,7 @@ class MainWindow(QWidget):
         super().__init__()
         self.setWindowTitle("Animal Detection System")
         self.setGeometry(100, 100, 1000, 500)
+        self.setFixedSize(1200, 850)
 
         self.last_detection = None
 
@@ -81,7 +139,19 @@ class MainWindow(QWidget):
             font-weight: bold;
         """)
 
-        # Container
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scroll_area.setFixedHeight(200)
+        self.scroll_area.setStyleSheet("""
+            QScrollArea {
+                background-color: #f8f9fb;
+                border-top: 1px solid #ddd;
+            }
+        """)
+
+        # ---- CONTRAINER ---- #
         title_label = QLabel("Result")
         title_label.setAlignment(Qt.AlignCenter)
         title_label.setStyleSheet("font-size: 20px; font-weight: bold;")
@@ -99,10 +169,48 @@ class MainWindow(QWidget):
         right_widget.setLayout(right_container)
         right_widget.setFixedWidth(350)
 
-        # main layout
-        main_layout = QHBoxLayout()
-        main_layout.addWidget(self.camera_label)
-        main_layout.addWidget(right_widget)
+        # ---- TOP SECTION (Camera + Info) ---- #
+        top_layout = QHBoxLayout()
+        top_layout.setContentsMargins(10, 10, 10, 5)
+
+        top_layout.addWidget(self.camera_label)
+        top_layout.addWidget(right_widget)
+
+        # ---- LOG SECTION ---- #
+        self.log_title = QLabel("Detection History")
+        self.log_title.setStyleSheet("""
+            font-size: 16px; 
+            font-weight: bold; 
+            color: #333; 
+            margin-left: 5px;
+            margin-bottom: 5px;
+        """)
+
+        self.log_scroll = QScrollArea()
+        self.log_scroll.setFixedHeight(180)
+        self.log_scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.log_scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.log_scroll.setWidgetResizable(True)
+
+        self.log_container = QWidget()
+
+        self.log_layout = QHBoxLayout()
+        self.log_layout.setContentsMargins(10, 10, 10, 10)
+        self.log_layout.setSpacing(15)
+        self.log_layout.setAlignment(Qt.AlignLeft)
+        self.log_layout.addStretch()
+
+        self.log_container.setLayout(self.log_layout)
+        self.log_scroll.setWidget(self.log_container)
+
+        # ---- MAIN LAYOUT ---- #
+        main_layout = QVBoxLayout()
+        main_layout.setContentsMargins(10, 10, 10, 10)
+        main_layout.setSpacing(10)
+
+        main_layout.addLayout(top_layout)
+        main_layout.addWidget(self.log_title)
+        main_layout.addWidget(self.log_scroll)
 
         self.setLayout(main_layout)
 
@@ -158,6 +266,9 @@ class MainWindow(QWidget):
             dist_info = "Distances: N/A"
 
         status_text = "KNOWN" if status else "NEW"
+        if not status:
+            timestamp = datetime.now().strftime("%H:%M:%S")
+            self.add_log_card(face_img, animal_class, timestamp)
 
         info_text = (
             f"Class: {animal_class.upper()}\n"
@@ -168,6 +279,46 @@ class MainWindow(QWidget):
         )
 
         self.info_label.setText(info_text)
+
+    # Add Card Logs (Only When NEW Information)
+    def add_log_card(self, img, label, timestamp):
+        card = QWidget()
+        card.setFixedSize(140, 140)
+        card.setStyleSheet("""
+            QWidget {
+                background-color: white;
+                border-radius: 10px;
+                border: 1px solid #ddd;
+            }
+        """)
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(5, 5, 5, 5)
+        layout.setSpacing(3)
+
+        img = cv2.resize(img, (100, 80))
+        rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb.shape
+        qt_img = QImage(rgb.data, w, h, ch * w, QImage.Format_RGB888)
+
+        img_label = QLabel()
+        img_label.setPixmap(QPixmap.fromImage(qt_img))
+        img_label.setAlignment(Qt.AlignCenter)
+
+        text_label = QLabel(f"{label.upper()}\n{timestamp}")
+        text_label.setAlignment(Qt.AlignCenter)
+        text_label.setStyleSheet("""
+            font-size: 10px;
+            color: #333;
+        """)
+
+        layout.addWidget(img_label)
+        layout.addWidget(text_label)
+
+        card.setLayout(layout)
+
+        self.log_layout.insertWidget(0, card)
+        self.log_scroll.horizontalScrollBar().setValue(0)
 
     # Close Event
     def closeEvent(self, event):
